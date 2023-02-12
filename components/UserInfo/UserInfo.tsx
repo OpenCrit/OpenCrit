251bc9d757ca2d099538c99102130bc7d0e680cf
Olivia <olivialiu200@gmail.com>
2023-02-12 18:27:17 -0500

    format

--- components/UserInfo/UserInfo.tsx
+++ components/UserInfo/UserInfo.tsx
@@ -1,32 +1,32 @@
 import styles from './UserInfo.module.css';
 
 export default function UserInfo({
-	name,
-	profileImage,
-	bio,
-	date,
-	points,
+    name,
+    profileImage,
+    bio,
+    date,
+    points,
 }: {
-	name: String;
-	profileImage: String;
-	bio: String;
-	date: String;
-	points: Number;
+    name: String;
+    profileImage: String;
+    bio: String;
+    date: String;
+    points: Number;
 }) {
-	return (
-		<div className={styles.UserInfo}>
-			<div className={styles.profilePicAndName}>
-				<img
-					src={profileImage as string}
-					className={styles.profilePic}
-				/>
-				<p className={styles.name}>{`@${name}`}</p>
-			</div>
-			<hr></hr>
-			<div className={styles.whiteBackground}>{`Bio: ${bio}`}</div>
-			<hr></hr>
-			<div className={styles.whiteBackground}>{`Date: ${date}`}</div>
-			<div className={styles.whiteBackground}>{`Points ${points}`}</div>
-		</div>
-	);
+    return (
+        <div className={styles.UserInfo}>
+            <div className={styles.profilePicAndName}>
+                <img
+                    src={profileImage as string}
+                    className={styles.profilePic}
+                />
+                <p className={styles.name}>{`@${name}`}</p>
+            </div>
+            <hr></hr>
+            <div className={styles.whiteBackground}>{`Bio: ${bio}`}</div>
+            <hr></hr>
+            <div className={styles.whiteBackground}>{`Date: ${date}`}</div>
+            <div className={styles.whiteBackground}>{`Points ${points}`}</div>
+        </div>
+    );
 }
