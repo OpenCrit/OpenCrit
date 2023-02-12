2666825cf19b416421a7cf01fa7cb440c452d1c4
Rebecca Lai <rebeccalai2003@gmail.com>
2023-02-12 16:34:44 -0500

    button styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import Button from 'components/Button/Button';
-import Navbar from 'components/Navbar/Navbar';
-import { Pane } from 'evergreen-ui';
+import Button from "components/Button/Button";
+import Navbar from "components/Navbar/Navbar";
+import { Pane } from "evergreen-ui";
 
 export default function Home() {
-    return (
-        <Pane>
-            <Button onClick={() => {}}>hi</Button>
+	return (
+		<Pane>
+			<Button onClick={() => {}}>Post</Button>
 
-            <Pane display="flex" justifyContent="center">
-                <Pane color="white" fontSize={26} margin="25%">
-                    Why hath you forsaken me.
-                </Pane>
-            </Pane>
-        </Pane>
-    );
+			<Pane display="flex" justifyContent="center">
+				<Pane color="white" fontSize={26} margin="25%">
+					Why hath you forsaken me.
+				</Pane>
+			</Pane>
+		</Pane>
+	);
 }
