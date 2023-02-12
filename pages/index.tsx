14cc6a542555d5b7684930cbb57d981beb055eb5
Ryan Chen <ryanchen54321@gmail.com>
2023-02-12 18:02:53 -0500

    formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Button from 'components/Button/Button';
 import Navbar from 'components/Navbar/Navbar';
-<<<<<<< HEAD
-=======
 import UserInfo from 'components/UserInfo/UserInfo';
->>>>>>> ecf2b5f85d6f3f18ba3a7460fd0196589c3e2243
 import SearchBar from 'components/SearchBar/SearchBar';
 import { Pane } from 'evergreen-ui';
 
@@ -12,12 +9,18 @@ export default function Home() {
         <Pane>
             <Button onClick={() => {}}>Post</Button>
             <SearchBar></SearchBar>
-			<Pane display="flex" justifyContent="center">
-				<Pane color="white" fontSize={26} margin="25%">
-					Why hath you forsaken me.
-				</Pane>
-			</Pane>
-      <UserInfo name={''} profileImage={''} bio={''} date={''} points={0}></UserInfo>
-		</Pane>
-	);
+            <Pane display="flex" justifyContent="center">
+                <Pane color="white" fontSize={26} margin="25%">
+                    Why hath you forsaken me.
+                </Pane>
+            </Pane>
+            <UserInfo
+                name={''}
+                profileImage={''}
+                bio={''}
+                date={''}
+                points={0}
+            ></UserInfo>
+        </Pane>
+    );
 }
