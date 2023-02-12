c3541e23af2e3287424f140c21e3cd7c7cc174b4
Olivia <olivialiu200@gmail.com>
2023-02-12 18:16:33 -0500

    formating

--- components/UserInfo/UserInfo.tsx
+++ components/UserInfo/UserInfo.tsx
@@ -1,33 +1,37 @@
-import styles from "./UserInfo.module.css";
+import styles from './UserInfo.module.css';
 
 interface UserInfoProps {
-    name: string;
-    profileImage: string;
-    bio: string;
-    date: string;
-    points: number;
-  }
+	name: string;
+	profileImage: string;
+	bio: string;
+	date: string;
+	points: number;
+}
 
-const UserInfo: React.FC<UserInfoProps> = ({ name, profileImage, bio, date, points}) => {
-    return (
-      <div className={styles.UserInfo}>
-        <div className={styles.profilepicAndName}>
-            <img src={profileImage} alt={name} className={styles.profilePic}/>
-            <p className={styles.name}>@{name}</p>
-        </div>
-        <hr></hr>
-        <div style={{ backgroundColor: "white" }}>
-            Bio: {bio}
-        </div>
-        <hr></hr>
-        <div style={{ backgroundColor: "white" }}>
-            Member since:{date}
-        </div>
-        <div style={{ backgroundColor: "white" }}>
-            Points:{points}
-        </div>
-      </div>
-    );
-  };
-  
-  export default UserInfo;
+const UserInfo: React.FC<UserInfoProps> = ({
+	name,
+	profileImage,
+	bio,
+	date,
+	points,
+}) => {
+	return (
+		<div className={styles.UserInfo}>
+			<div className={styles.profilepicAndName}>
+				<img
+					src={profileImage}
+					alt={name}
+					className={styles.profilePic}
+				/>
+				<p className={styles.name}>@{name}</p>
+			</div>
+			<hr></hr>
+			<div style={{ backgroundColor: 'white' }}>Bio: {bio}</div>
+			<hr></hr>
+			<div style={{ backgroundColor: 'white' }}>Member since:{date}</div>
+			<div style={{ backgroundColor: 'white' }}>Points:{points}</div>
+		</div>
+	);
+};
+
+export default UserInfo;
