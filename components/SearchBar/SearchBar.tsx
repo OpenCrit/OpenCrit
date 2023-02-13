aa70d5a48a6d582ea8ad8ba1751bd179492ca25e
Rebecca Lai <rebeccalai2003@gmail.com>
2023-02-13 03:11:37 -0500

    waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa done search bar

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,60 +1,70 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-	faMagnifyingGlass,
-	faChevronDown,
+    faMagnifyingGlass,
+    faChevronDown,
 } from '@fortawesome/free-solid-svg-icons';
 import styles from './SearchBar.module.css';
 import SearchTag from 'components/SearchTag/SearchTag';
 
 export default function SearchBar() {
-	const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-	const searchTags = [
-		{ name: 'Painting', color: '#000000' },
-		{ name: 'Illustration', color: '#000000' },
-		{ name: 'Sculpture', color: '#000000' },
-		{ name: 'Animation', color: '#000000' },
-		{ name: 'Design', color: '#000000' },
-		{ name: 'Video', color: '#000000' },
-		{ name: 'Fashion', color: '#000000' },
-		{ name: 'Fiber', color: '#000000' },
-		{ name: 'Others', color: '#000000' },
-	];
+    const searchTags = [
+        { name: 'Painting', color: '#ffa9a9' },
+        { name: 'Illustration', color: '#efb190' },
+        { name: 'Sculpture', color: '#9bd634' },
+        { name: 'Animation', color: '#39ec39' },
+        { name: 'Design', color: '#39eccb' },
+        { name: 'Video', color: '#3995ec' },
+        { name: 'Fashion', color: '#393fec' },
+        { name: 'Fiber', color: '#8739ec' },
+        { name: 'Others', color: '#d139ec' },
+    ];
 
-	const handleDropdown = () => {
-		setIsOpen(!isOpen);
-	};
+    const handleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.topContainer}>
-				<FontAwesomeIcon
-					icon={faMagnifyingGlass}
-					className={styles.magnifyingGlass}
-				/>
-				<input
-					className={styles.searchBar}
-					type="text"
-					placeholder="Search Posts"
-				/>
-				<FontAwesomeIcon
-					icon={faChevronDown}
-					className={styles.dropDown}
-					onClick={handleDropdown}
-				/>
-			</div>
-			{isOpen ? (
-				<div className={styles.bottomContainer}>
-					{searchTags.map((tagObject) => (
-						<div>
-							<SearchTag color={tagObject['color']}>
-								{tagObject['name']}
-							</SearchTag>
-						</div>
-					))}
-				</div>
-			) : null}
-		</div>
-	);
+    const topContainerStyle = isOpen
+        ? styles.topContainerOpen
+        : styles.topContainer;
+
+    return (
+        <div className={styles.container}>
+            <div className={topContainerStyle}>
+                <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    className={styles.magnifyingGlass}
+                />
+                <input
+                    className={styles.searchBar}
+                    type="text"
+                    placeholder="Search Posts"
+                />
+                <FontAwesomeIcon
+                    icon={faChevronDown}
+                    className={styles.dropDown}
+                    onClick={handleDropdown}
+                />
+            </div>
+            {isOpen ? (
+                <div
+                    className={styles.bottomContainer}
+                    style={{
+                        backgroundColor: '#e9e9e9',
+                        border: 'none',
+                    }}
+                >
+                    {searchTags.map((tagObject) => (
+                        <div>
+                            <SearchTag color={tagObject['color']}>
+                                {tagObject['name']}
+                            </SearchTag>
+                        </div>
+                    ))}
+                </div>
+            ) : null}
+        </div>
+    );
 }
