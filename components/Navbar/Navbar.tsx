d277007dadff420c4e9c2c95abf3d543edc2153b
Ryan Chen <ryanchen54321@gmail.com>
2023-02-16 16:57:12 -0500

    WOoOoO

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,10 +2,16 @@ import Button from 'components/Button/Button';
 import SearchBar from 'components/SearchBar/SearchBar';
 import styles from './Navbar.module.css';
 import DropdownButton from 'components/DropdownButton/DropdownButton';
+import SignIn from 'components/SignIn/SignIn';
+import { useState } from 'react';
 
 export default function Navbar() {
+    const [signInActive, setSignInActive] = useState(false);
+
     const handlePost = () => {};
-    const handleLogIn = () => {};
+    const handleLogin = () => {
+        setSignInActive(true);
+    };
 
     return (
         <div className={styles.navbarContainer}>
@@ -17,9 +23,14 @@ export default function Navbar() {
             </div>
             <div className={styles.buttonContainer}>
                 <Button onClick={handlePost}>Post</Button>
-                <Button onClick={handleLogIn}>Log In</Button>
+                <Button onClick={handleLogin}>Log In</Button>
                 <DropdownButton />
             </div>
+            {signInActive ? (
+                <div className={styles.signIn}>
+                    <SignIn />
+                </div>
+            ) : null}
         </div>
     );
 }
