fd18d663ddd1cc3b6de6f12b5b30a0c6a8826bc7
Ryan Chen <58120896+RyanChenSWE@users.noreply.github.com>
2023-02-16 20:11:55 -0500

    Update components/SignUpBackground/SignUpBackground.tsx

--- components/SignUpBackground/SignUpBackground.tsx
+++ components/SignUpBackground/SignUpBackground.tsx
@@ -3,7 +3,7 @@ import PaintblobRed2 from '../../public/svg/PaintblobRed2.svg';
 import PaintblobBlue2 from '../../public/svg/PaintblobBlue2.svg';
 import PaintblobYellow2 from '../../public/svg/PaintblobYellow2.svg';
 
-export default function SignIn() {
+export default function SignUpBackground() {
     return (
         <div className={styles.signUpBackground}>
             <img src={PaintblobBlue2.src} className={styles.paintBlue} />
